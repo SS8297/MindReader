a1da938180a7631f382d640b08ce385de4cf15ff
Shuai Shao <shuai1997@hotmail.se>
2023-03-14 13:48:55 +0100

    Set model initialization to seed 31415

--- src/Architect/architect.jl
+++ src/Architect/architect.jl
@@ -21,6 +21,7 @@ See also: [`modelTrain!`](@ref)
 function buildAutoencoder(inputLayer::I; nnParams) where I <: Integer
   @info("Building three-layered autoencoder...")
   args = nnParams()
+  Random.seed(31415);
   return Chain(
     Dense(inputLayer, args.λ, args.σ),
     Dense(args.λ, inputLayer, args.σ),
