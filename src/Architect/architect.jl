040d734e4c4f8f8e19f380908a79483d36805154
Shuai Shao <shuai1997@hotmail.se>
2023-04-24 11:59:56 +0200

    Implemented CNN architecture

--- src/Architect/architect.jl
+++ src/Architect/architect.jl
@@ -22,7 +22,7 @@ Build a three-layered autoencoder.
 See also: [`modelTrain!`](@ref)
 """
 function buildAutoencoder(inputLayer::I; nnParams) where I <: Integer
-  @info("Building three-layered autoencoder...")
+  # @info("Building three-layered autoencoder...")
   args = nnParams()
   num_lay = args.l + 1
   model = Array{Dense}(undef, num_lay)
@@ -39,3 +39,44 @@ function buildAutoencoder(inputLayer::I; nnParams) where I <: Integer
 end
 
 ####################################################################################################
+
+# convolutional autoencoder
+"""
+
+    buildAutoencoder(inputLayer::I;
+    nnParams)
+    where I <: Integer
+
+# Description
+Build a three-layered autoencoder.
+
+# Arguments
+`inputLayer` number of neurons on input.
+
+`nnParams` neural network hyperparameters.
+
+
+See also: [`modelTrain!`](@ref)
+"""
+function buildAutoencoder(inputLayer::I; nnParams) where I <: Integer
+  # @info("Building three-layered autoencoder...")
+  args = nnParams()
+  kw = args.KW
+  kh = args.KH
+  ci = args.CI
+  co = args.CO
+  ks = args.KS
+  zp = args.ZP
+  num_lay = args.l + 1
+  encode = Array{Conv}(undef, length(kw))
+  decode = Array{Conv}(undef, length(kw))
+  for i in eachindex(kw)
+    j = length(kw) - i + 1
+    seed!(31415);
+    encode[i] = Conv((kw[i], kh[i]), ci[i] => co[i], args.σ; init = kaiming_normal, stride = ks[i], pad = zp[i])
+    seed!(31415);
+    decode[j] = ConvTranspose((kw[j], kh[j]), ci[j] => co[j], args.σ; init = kaiming_normal, stride = ks[j], pad = zp[j])
+  end
+  return Chain(encode..., decode...)
+end
+
